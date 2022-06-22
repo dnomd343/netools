70a65ea7f8efa3d4318610b1c6584c87dd9498dc
dnomd343 <i@343.re>
2022-06-22 23:33:10 +0800

    update: web api of ping and tcping

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 
+import json
 from tools import *
-from flask import Flask, request
+from flask import Flask, Response, request
 
 apiPath = '/'
 api = Flask(__name__)
@@ -21,25 +22,65 @@ def httpPostArg(field: str) -> dict or str or None: # get HTTP POST param
     return None
 
 
+def httpArgument(args: list) -> dict:
+    result = {}
+    for arg in args:
+        if request.method == 'GET':
+            result[arg] = request.args.get(arg)
+        elif request.method == 'POST':
+            result[arg] = httpPostArg(arg)
+    return result
+
+
+def responseJson(data: dict) -> Response:
+    return Response(json.dumps(data), mimetype = 'application/json')
+
+
+def toInt(raw) -> int or None:
+    if raw is None:
+        return raw
+    if isinstance(raw, (int, float)): # int / float -> int
+        return int(raw)
+    elif isinstance(raw, bytes): # bytes -> str
+        raw = str(raw, encoding = 'utf-8')
+    elif not isinstance(raw, str):
+        raise Exception('type not allowed')
+    try:
+        return int(raw)
+    except:
+        raise Exception('not a integer')
+
+
 @api.route(apiPath + '/ping', methods = ['GET', 'POST'])
-def pingMethod() -> dict:
-    if request.method == 'GET':
-        try:
-            return {
-                'success': True,
-                **ping.ping(
-                    request.args.get('server'),
-                    request.args.get('v6First'),
-                    request.args.get('count'),
-                    request.args.get('fast'),
-                    request.args.get('size'),
-                    request.args.get('timeout'),
-                )
-            }
-        except:
-            pass
-    elif request.method == 'POST':
-        pass
+def pingMethod() -> Response:
+    args = httpArgument(['server', 'v6First', 'count', 'fast', 'size', 'timeout'])
+    try:
+        return responseJson({
+            'success': True,
+            **ping.ping(args['server'], args['v6First'], toInt(args['count']),
+                        args['fast'], toInt(args['size']), toInt(args['timeout']))
+        })
+    except Exception as exp:
+        return responseJson({
+            'success': False,
+            'message': str(exp)
+        })
+
+
+@api.route(apiPath + '/tcping', methods = ['GET', 'POST'])
+def tcpingMethod() -> Response:
+    args = httpArgument(['server', 'port', 'v6First', 'count', 'timeout'])
+    try:
+        return responseJson({
+            'success': True,
+            **tcping.tcping(args['server'], toInt(args['port']), args['v6First'],
+                            toInt(args['count']), toInt(args['timeout']))
+        })
+    except Exception as exp:
+        return responseJson({
+            'success': False,
+            'message': str(exp)
+        })
 
 
 api.run(host = '0.0.0.0', port = 80, debug = True)
