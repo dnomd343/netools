f839d51e8371f0a18e3980864b3eea1abc74b473
Dnomd343 <i@343.re>
2022-10-20 20:31:15 +0800

    build: update best-trace script

--- best-trace.sh
+++ best-trace.sh
@@ -3,31 +3,13 @@
 wget https://cdn.ipip.net/17mon/besttrace4linux.zip
 unzip besttrace4linux.zip
 
-# get machine architecture
 case "$(uname -m)" in
-  'i386' | 'i686')
-    ARCH='i386';;
-  'amd64' | 'x86_64')
-    ARCH='amd64';;
-  'armv7' | 'armv7l')
-    ARCH='arm';;
-  'armv8' | 'aarch64')
-    ARCH='arm64';;
+  'i386' | 'i686' | 'amd64' | 'x86_64')
+    BEST_TRACE='besttrace32';;
+  'armv7' | 'armv7l' | 'armv8' | 'aarch64')
+    BEST_TRACE="besttracearm";;
   *)
   echo "Architecture not supported" && exit 1
-esac
-
-case "${ARCH}" in
-  'i386')
-    BEST_TRACE='besttrace32';;
-  'amd64')
-    BEST_TRACE='besttrace32';;
-  'arm')
-    BEST_TRACE="besttracearm";;
-  'arm64')
-    BEST_TRACE="besttracearm";;
-  *)
-  exit 1
 esac
 
 mv ${BEST_TRACE} /tmp/besttrace
