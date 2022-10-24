d177fcc527a222e4679a836d82cbe45cd1ef3620
Dnomd343 <i@343.re>
2022-10-24 15:30:11 +0800

    update: enhance tcping module

--- tcping/tcping.py
+++ tcping/tcping.py
@@ -17,6 +17,7 @@ class TCPing:
         server: TCPing package target. (ipv4 / ipv6 / domain)
         port: TCP port for connection. (1 ~ 65535)
         v6First: IPv6 is preferred. (server -> domain name)
+        fast: TCPing as soon as reply is received.
         count: The number of tcp connection tried. (1 ~ 16)
         timeout: Time limit for each connection. (1 ~ 10)
     """
@@ -24,24 +25,27 @@ class TCPing:
         ('server', str, isHost),
         ('port', int, isPort),
         ('v6First', bool, None),
+        ('fast', bool, None),
         ('count', int, lambda x: 1 <= x <= 16),
         ('timeout', int, lambda x: 1 <= x <= 10),
     ]
-
-    def __valueInit(self) -> None:  # load default values
-        self.v6First = False
-        self.count = 4
-        self.timeout = 3
+    server = None  # load default values
+    port = None
+    v6First = False
+    fast = True
+    count = 4
+    timeout = 3
 
     def __valueCheck(self) -> None:  # check parameter values
         checker('TCPing', self.rules,
-            self.server, self.port, self.v6First, self.count, self.timeout
+            self.server, self.port, self.v6First, self.fast, self.count, self.timeout
         )
 
     def __valueDump(self) -> None:  # output parameter values
         logger.debug('[%s] TCPing server -> %s' % (self.id, self.server))
         logger.debug('[%s] TCPing port -> %d' % (self.id, self.port))
         logger.debug('[%s] TCPing v6First -> %s' % (self.id, self.v6First))
+        logger.debug('[%s] TCPing fast -> %s' % (self.id, self.fast))
         logger.debug('[%s] TCPing count -> %d' % (self.id, self.count))
         logger.debug('[%s] TCPing timeout -> %d' % (self.id, self.timeout))
 
@@ -50,11 +54,13 @@ class TCPing:
         tcpingCmd = [
             'tcping', self.server, str(self.port),
             '--counter', str(self.count),
-            '--timeout', '%ds' % self.timeout
+            '--timeout', '%ds' % self.timeout,
         ]
+        if self.fast:  # enable fast mode
+            tcpingCmd += ['--interval', '1ns']
         logger.debug('[%s] TCPing command -> %s' % (self.id, tcpingCmd))
         process = runProcess(self.id, tcpingCmd, None)
-        process.wait()  # wait ping process exit
+        process.wait()  # wait tcping process exit
         output = process.stdout.read().decode()
         logger.debug('[%s] TCPing raw output ->\n%s' % (self.id, output))
         return output
@@ -86,24 +92,24 @@ class TCPing:
             'ip': self.server,  # actual address
             'port': self.port,
             'alive': True,  # server online
-            'statistics': {
+            'result': {
+                'raw': result,  # raw latency result
                 'count': self.count,  # number of transmit tcping
                 'reply': len(result),  # number of successful tcping
-                # TODO: result analyse (rate / avg / cv ...)
-        #         'rate': format(len(tcpingResult) / count * 100, '.1f') + '%',  # success rate
-        #         **basis.getArrangeInfo(tcpingResult)
+                'rate': '%s%%' % format(len(result) / self.count * 100, '.1f'),  # success rate
+                # TODO: result analyse -> avg / cv ...
             }
         }
 
     def __init__(self, server: str, port: int) -> None:
         self.id = genFlag()
-        self.__valueInit()
-        self.server = server  # load ping server
+        self.server = server  # load tcping server
         self.port = port # load tcping port
         logger.debug('[%s] TCPing task init -> %s(:%d)' % (self.id, self.server, self.port))
 
     def run(self) -> dict:
         self.__valueCheck()
+        # TODO: load request values
         self.server = host2IP(self.server, self.v6First)  # convert into ip address
         logger.info('[%s] TCPing task -> %s(:%d)' % (self.id, self.server, self.port))
         self.__valueDump()
