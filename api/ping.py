69aafd3a648a3f4b7b30a4038ecaaa51c09636e7
Dnomd343 <i@343.re>
2022-10-24 18:52:17 +0800

    update: ping check method

--- api/ping.py
+++ api/ping.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+from ping import Ping
+from utils import logger
 from flask import Response
 from api.utils import webApi
 from api.utils import tokenCheck
+from api.utils import httpArgument
 from api.utils import jsonResponse
+from api.utils import toInt, toBool
 
 
 def pingMethod() -> dict:
     if not tokenCheck():
         raise RuntimeError('Invalid token')
-    return {
-        'test': 'ok'
-    }
+    pingArgs = httpArgument(['server', 'v6First', 'count', 'fast', 'size', 'timeout'])
+    if pingArgs['server'] is None:
+        raise RuntimeError('Missing `server` option')
+    ping = Ping(pingArgs['server'])
+
+    # TODO: combine check args process
+    if pingArgs['v6First'] is not None:
+        ping.v6First = toBool('v6First', pingArgs['v6First'])
+    if pingArgs['count'] is not None:
+        ping.count = toInt('count', pingArgs['count'])
+    if pingArgs['fast'] is not None:
+        ping.fast = toBool('fast', pingArgs['fast'])
+    if pingArgs['size'] is not None:
+        ping.size = toBool('size', pingArgs['size'])
+    if pingArgs['timeout'] is not None:
+        ping.timeout = toInt('timeout', pingArgs['timeout'])
+
+    return ping.run()
 
 
 @webApi.route('/ping', methods = ['GET', 'POST'])
@@ -23,20 +42,8 @@ def apiPing() -> Response:
             **pingMethod(),
         })
     except Exception as exp:
+        logger.error('Ping request error -> %s' % exp)
         return jsonResponse({
             'success': False,
             'message': str(exp),
         })
-    # args = httpArgument(['server', 'v6First', 'count', 'fast', 'size', 'timeout'])
-    # try:
-    #     return responseJson({
-    #         'success': True,
-    #         **ping.ping(args['server'], toBool(args['v6First']), toInt(args['count']),
-    #                     toBool(args['fast']), toInt(args['size']), toInt(args['timeout']))
-    #     })
-    # except Exception as exp:
-    #     return responseJson({
-    #         'success': False,
-    #         'message': str(exp)
-    #     })
-
--- api/utils.py
+++ api/utils.py
@@ -45,3 +45,37 @@ def tokenCheck() -> bool:
     if clientToken is None or clientToken != ApiToken:  # invalid token
         return False
     return True
+
+
+def toInt(prefix: str, raw) -> int:  # convert to int
+    if isinstance(raw, (int, float)):  # int / float -> int
+        return int(raw)
+    elif isinstance(raw, bytes):  # bytes -> str
+        raw = str(raw, encoding = 'utf-8')
+    elif not isinstance(raw, str):
+        raise RuntimeError('Field `%s` type not allowed' % prefix)
+    try:
+        return int(raw)
+    except:
+        raise RuntimeError('Field `%s` not a integer' % prefix)
+
+
+def toBool(prefix: str, raw) -> bool:  # convert to bool
+    if isinstance(raw, bool):  # bool -> bool
+        return raw
+    if isinstance(raw, int):  # int -> str
+        raw = str(raw)
+    elif isinstance(raw, bytes):  # bytes -> str
+        raw = str(raw, encoding = 'utf-8')
+    elif not isinstance(raw, str):
+        raise RuntimeError('Field `%s` type not allowed' % prefix)
+    raw = raw.strip().lower()
+    if raw == 'true':
+        return True
+    elif raw == 'false':
+        return False
+    try:
+        raw = int(raw)
+        return raw != 0
+    except:
+        raise RuntimeError('Field `%s` not a boolean' % prefix)
