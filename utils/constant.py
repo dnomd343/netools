3ca111bcbf34d541d71021d5ba1021a0676b8cbd
Dnomd343 <i@343.re>
2022-10-25 00:04:31 +0800

    update: modify api token from env

--- utils/constant.py
+++ utils/constant.py
@@ -3,10 +3,14 @@
 
 # TODO: load options from env vars and start params
 
+import os
+
 Version = '0.0.9'
 
 ApiPort = 5633
 ApiToken = 'dnomd343'
+if 'token' in os.environ:
+    ApiToken = os.environ['token']
 
 LogLevel = 'debug'
 LogFile = 'runtime.log'
